c106fe107c10700c24be56faca5b762a9ac87a29
Rafael Carvalho <rafaeldecarvalhodasilva01@yahoo.com.br>
2021-08-09 13:49:20 -0300

    Início

---
+++ interacao.js
@@ -0,0 +1,10 @@
+function prosseguir() {
+    var nome = document.getElementById('nome')
+    var nascimento = document.getElementById('nascimento')
+    var email = document.getElementById('email')
+    var telefone = document.getElementById('telefone')
+    var cor = document.getElementById('cor')
+    var sexom = document.getElementById('sexom')
+    var sexof = document.getElementById('sexof')
+    }
+}
